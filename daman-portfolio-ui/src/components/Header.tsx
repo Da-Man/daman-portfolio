9955cca074d0159ad5a65db9a446c7b303101f66
Daman <damanvir2singh@gmail.com>
2024-10-18 21:00:21 +1100

    quality improvements

--- daman-portfolio-ui/src/components/Header.tsx
+++ daman-portfolio-ui/src/components/Header.tsx
@@ -5,6 +5,15 @@ const Header: React.FC = () => {
   return (
     <div className="Header">
         <a href="#top" className="Header-title">// Damanvir Singh</a>
+
+        <nav>
+          <a href="#ICanCode">Reason #1</a>
+          <a href="#HTMLDemo">HTML Demo</a>
+          <a href="#CSSDemo">CSS Demo</a>
+          <a href="#JavaDemo">Java Demo</a>
+          <a href="#PHPDemo">PHP Demo</a>
+          <a href="#IHaveExperience">Reason #2</a>
+        </nav>
     </div>
   )
 }
